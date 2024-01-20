905a7f4cd6a2e4257d4d0f2fcb8dc31d3e94766d
Mario Hernandez <mhmario963@gmail.com>
2024-01-20 16:28:21 -0500

    update tests.

--- app/src/test/java/org/mariocoding/utilities/ArgumentParserTest.java
+++ app/src/test/java/org/mariocoding/utilities/ArgumentParserTest.java
@@ -9,25 +9,58 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class ArgumentParserTest {
+    private static final char TAB = '\t';
+    private static final char COMMA = ',';
+    private static final String FILE_NAME = "fileName";
+
     @ParameterizedTest
-    @MethodSource("inputsAndResults")
-    public void parseFieldListWithValidValues(String argument, List<Integer> expectedFieldList) {
-        ArgumentParser argumentParser = new ArgumentParser(new String[] {argument, "fileName"});
+    @MethodSource("fieldListInputsAndResults")
+    public void constructorWhenValidFieldListAndFileNameConstructsObject(String argument, List<Integer> expectedFieldList, char expectedDelimiter) {
+        ArgumentParser argumentParser = new ArgumentParser(new String[]{argument, "fileName"});
 
         List<Integer> actualFieldList = argumentParser.getFieldList();
 
         for (int i = 0; i < expectedFieldList.size(); i++) {
             Assertions.assertEquals(expectedFieldList.get(i).intValue(), actualFieldList.get(i).intValue());
         }
+
+        Assertions.assertEquals(expectedDelimiter, argumentParser.getDelimiter().charValue());
+        Assertions.assertEquals(FILE_NAME, argumentParser.getFileName());
     }
 
-    private static Stream<Arguments> inputsAndResults() {
+    @ParameterizedTest
+    @MethodSource("fieldListAndDelimiterInputsAndResults")
+    public void constructorWithDelimiter(String[] arguments, List<Integer> expectedFieldList, char expectedDelimiter) {
+        ArgumentParser argumentParser = new ArgumentParser(arguments);
+
+        List<Integer> actualFieldList = argumentParser.getFieldList();
+
+        Assertions.assertEquals(expectedDelimiter, argumentParser.getDelimiter());
+
+        for (int i = 0; i < expectedFieldList.size(); i++) {
+            Assertions.assertEquals(expectedFieldList.get(i).intValue(), actualFieldList.get(i).intValue());
+        }
+
+        Assertions.assertEquals(FILE_NAME, argumentParser.getFileName());
+    }
+
+    private static Stream<Arguments> fieldListInputsAndResults() {
         return Stream.of(
-                Arguments.of("-f1", List.of(1)),
-                Arguments.of("-f 1", List.of(1)),
-                Arguments.of("-f1,2,3", List.of(1,2,3)),
-                Arguments.of("-f \"1,2,3\"", List.of(1,2,3)),
-                Arguments.of("-f 1,2,3", List.of(1,2,3))
+                Arguments.of("-f1", List.of(1), TAB),
+                Arguments.of("-f 1", List.of(1), TAB),
+                Arguments.of("-f1,2,3", List.of(1, 2, 3), TAB),
+                Arguments.of("-f \"1,2,3\"", List.of(1, 2, 3), TAB),
+                Arguments.of("-f 1,2,3", List.of(1, 2, 3), TAB)
+        );
+    }
+
+    private static Stream<Arguments> fieldListAndDelimiterInputsAndResults() {
+        return Stream.of(
+                Arguments.of(new String[]{"-f1", "-d,", FILE_NAME}, List.of(1), COMMA),
+                Arguments.of(new String[]{"-f1", "-d ,", FILE_NAME}, List.of(1), COMMA),
+                Arguments.of(new String[]{"-f1,2,3", "-d \",\"", FILE_NAME}, List.of(1, 2, 3), COMMA),
+                Arguments.of(new String[]{"-f1,2,3", "-d\",\"", FILE_NAME}, List.of(1, 2, 3), COMMA),
+                Arguments.of(new String[]{"-f1,2,3", "-d\",\"", FILE_NAME}, List.of(1, 2, 3), COMMA)
         );
     }
 }

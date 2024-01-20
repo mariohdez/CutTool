14c249933c7bb3dc35c89b097d6d9431f849dc41
Mario Hernandez <mhmario963@gmail.com>
2024-01-19 20:52:31 -0500

    add intelliJ workspace to git ignore.

--- src/main/java/org/mariocoding/Cut.java
+++ src/main/java/org/mariocoding/Cut.java
@@ -5,6 +5,5 @@ import org.mariocoding.utilities.ArgumentParser;
 public class Cut {
     public static void main(String[] args) {
         ArgumentParser argumentParser = new ArgumentParser(args);
-        System.out.println(argumentParser);
     }
 }
